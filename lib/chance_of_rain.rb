d4c9b3c47814db2c8cf6a08f27949090b76e0692
munificent-forum-1695 <bunchshawn@gmail.com>
2020-09-22 18:56:23 +0000

    Automatically backed up by Learn

---
+++ lib/chance_of_rain.rb
@@ -0,0 +1,11 @@
+puts "What is the chance of rain? Enter percent chance as a decimal."
+
+chance_of_rain = gets.strip.to_i
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
+  
