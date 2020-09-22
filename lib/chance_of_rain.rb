08dcea4d59bea026ce8b365f11a0c9a342a83252
munificent-forum-1695 <bunchshawn@gmail.com>
2020-09-22 19:03:04 +0000

    Automatically backed up by Learn

--- lib/chance_of_rain.rb
+++ lib/chance_of_rain.rb
@@ -1,6 +1,6 @@
 puts "What is the chance of rain? Enter percent chance of rain"
 
-chance_of_rain = (gets.strip.to_i / 1000)
+chance_of_rain = gets.strip.to_f
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
